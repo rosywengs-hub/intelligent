24d7e538f0a7c027809958cae7e966a697e4d898
rosywengs-hub <rosywengs@gmail.com>
2025-09-25 18:11:58 +0100

    Add App.js component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Mindboost</h1>
+      <p>Intelligent Support System for National Competitive Exams in Cameroon</p>
+    </div>
+  );
+}
+
+export default App;
